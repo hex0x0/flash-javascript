1619592ecf528422ec04f37b24123846347d3c07
lucas <lucasucb2020@gmail.com>
2022-04-01 18:05:52 -0300

    Forma mais simples de dar merge em objetos

--- objects.js
+++ objects.js
@@ -80,3 +80,51 @@ let objPessoaGato = Object.assign({}, pessoa, objGato);
 
 console.log(objPessoaGato.cachorro.cor);
 
+
+//ou
+
+let objetoPessoaModificado = Object.assign(pessoa, objGato);//objeto pessoa recebe as propriedades do objGato
+
+
+
+//Forma mais simples de fazer o merge de objetos
+
+
+let ob1 = {
+    dvd_favorito:'calcinha preta'
+};
+
+let ob2 = {
+    filme_favorito:'velozes e furiosos'
+};
+
+//Criamos um objeto que tem mais dois objetos dentro dele, mas não fizemos a junção propriamente
+//dita dos elementos
+
+/*
+ob3 = {
+    ob1,
+    ob2
+}
+
+console.log(ob3);
+*/
+
+
+let ob3 = {
+    ...ob1,
+    ...ob2
+};
+
+
+console.log(ob3); 
+
+//Sempre que eu quiser que o nome da minha propriedade seja o valor de uma variável, basta
+//colocá-lo entre []
+
+let variavel = 'estado';
+let teste = {
+    [variavel]:'rio de huezeiro'
+};
+
+
