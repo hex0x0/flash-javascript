b0f3b16c6049d499f788dcb6df064fdd5c0ebc43
lucas <lucasucb2020@gmail.com>
2022-04-04 13:49:20 -0300

    Constant or just const

---
+++ constant.js
@@ -0,0 +1,26 @@
+const anoNascimento = 1995;
+
+//Só é visível no bloco em que foi declarada
+//Você não pode nem redeclarar ou reatribuir no mesmo escopo
+
+
+//const anoNascimento = 200;
+
+
+if(true){
+    const idade = 12;
+}
+
+//console.log(idade);
+const idade = 14;
+console.log(idade);
+
+
+const lang = ['java', 'python', 'c'];
+
+lang[0] = 'kotlin';
+
+console.log(lang);
+
+
+
