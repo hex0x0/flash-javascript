84864ea0f1312d5a9ec19b4e827d9cb5b9c9d26c
lucas <lucasucb2020@gmail.com>
2022-04-04 12:28:49 -0300

    Usage of ArrowFunctions

---
+++ arrowFunction.js
@@ -0,0 +1,40 @@
+let soma = (a, b) =>{
+    return a + b;
+}
+
+
+console.log(soma(2, 4));
+
+
+
+
+let mult = (a, b) => a*b;
+
+
+console.log(mult(3, 4));
+
+
+//Retornando objetos
+
+let returnUser = () => (
+
+    {
+        nome:'ana',
+        status:'casada'
+    }
+
+)
+
+console.log(returnUser());
+
+
+let addServer = (cidade) => (
+    {
+        cidade
+    }
+
+)
+
+console.log(addServer('brasília'));
+
+
